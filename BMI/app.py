e1c71058e910d3b0794164f8a4fcc2fb2f1d273a
Chirag Vaviya <79510865+chirag8850@users.noreply.github.com>
2023-04-28 11:14:37 +0530

    Update app.py

--- BMI/app.py
+++ BMI/app.py
@@ -1,14 +1,19 @@
+#pip install Flask
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    bmi = ''
-    if request.method == 'POST' and 'weight' in request.form:
-        weight = float(request.form.get('weight'))
-        height = float(request.form.get('height'))
-        bmi = calc_bmi(weight, height)
-    return render_template("index.html",bmi=bmi)
-def calc_bmi(weight, height):
-    return round((weight / ((height / 100) ** 2)), 2)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    weight = float(request.form['weight'])
+    height = float(request.form['height'])
+    bmi = round(weight / (height ** 2), 2)
+    return render_template('index.html', bmi=bmi)
+
 if __name__ == '__main__':
     app.run(debug=True)
+#python app.py
